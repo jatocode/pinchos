19cf2775217243f7c039a5c4fa90a23206b251d9
Tobias Jansson <tobias.jansson@gmail.com>
2018-04-25 18:10:13 +0200

    Första utkast på Pinchos appen

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+export class App {
+  message = 'Tyras Pinchos!';
+
+  drycker = ["Mjölk", "Vatten", "Saft", "Coca cola", "Sprite"];
+  matratter = ["Köttbullar", "Kycklingspett", "Risotto", "Soppa", "Enchiladas", "Nachos"];
+}
