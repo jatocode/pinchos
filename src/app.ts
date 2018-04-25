e4227521db160e338a7ab09a7510da44653f7a9c
Tobias Jansson <tobias.jansson@gmail.com>
2018-04-25 22:03:53 +0200

    Beställning

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { Enhet } from "./enhet";
 export class App {
   message = 'Tyras Pinchos!';
+  namn = "";
+  orders = [];
 
   drycker = ["Mjölk", "Vatten", "Saft", "Coca cola", "Sprite"];
   matratter = ["Pizza", "Minihamburgare","Köttbullar", "Kycklingspett", "Risotto", "Soppa", "Enchiladas", "Nachos"];
@@ -15,4 +17,17 @@ export class App {
   public minska(en:Enhet) {
     en.antal == 0? en.antal = 0: en.antal--;
   }
+
+  public bestall() {
+    let order = {"namn": this.namn,
+                 "dryck": this.dryckenheter.filter(x => x.antal > 0),
+                 "mat": this.matenheter.filter(x => x.antal > 0) 
+                };
+    this.orders.push(order);
+
+    //this.dryckenheter.forEach(x => x.antal = 0);
+    //this.matenheter.forEach(x => x.antal = 0);
+    this.namn = "";
+  }
+
 }
